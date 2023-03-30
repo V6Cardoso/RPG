07a142a4383334202cd83896fe7a458fe03080b6
Victor Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2023-03-30 02:03:24 -0300

    added voice support

---
+++ static/js/speech.js
@@ -0,0 +1,46 @@
+const recordButton = document.getElementById('record-button');
+const stopRecordingButton = document.getElementById('stop-recording-button');
+const inputText = document.getElementById("search-input");
+
+class speechApi {
+
+    constructor() {
+  
+      const SpeechToText = window.SpeechRecognition || window.webkitSpeechRecognition
+  
+      this.speechApi = new SpeechToText()
+      this.output = inputText.output 
+      this.speechApi.continuous = true
+      this.speechApi.lang = "pt-BR"
+      
+      this.speechApi.onresult = (e) => {
+        var resultIndex = e.resultIndex
+        var transcript = e.results[resultIndex][0].transcript
+  
+        inputText.value += transcript.replace('.', '')
+      }
+    }
+  
+    start() {
+      this.speechApi.start()
+    }
+  
+    stop() {
+      this.speechApi.stop()
+    }
+}
+
+var speech = new speechApi();
+
+function record() {
+    stopRecordingButton.hidden = false;
+    recordButton.hidden = true;
+    inputText.value = '';
+    speech.start();
+}
+
+function stopRecording() {
+    recordButton.hidden = false;
+    stopRecordingButton.hidden = true;
+    speech.stop();
+}
